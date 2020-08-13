7d5319b47cdbbc207d6efd09586f567ec81e0354
USER <user@USERnoMacBook-Pro.local>
2020-08-13 22:05:48 +0900

    Updates

---
+++ precache-manifest.3f654d759069e82feee30af565eb18e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4caa4c80ba733453b8b5e9ab705fb8c7",
+    "url": "/home-ops/index.html"
+  },
+  {
+    "revision": "88f4f07bbfa588ea4ef6",
+    "url": "/home-ops/static/css/main.3c66f03f.chunk.css"
+  },
+  {
+    "revision": "50f9f48673a0b47ee37d",
+    "url": "/home-ops/static/js/2.98489b16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/home-ops/static/js/2.98489b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f4f07bbfa588ea4ef6",
+    "url": "/home-ops/static/js/main.2f8cbdc8.chunk.js"
+  },
+  {
+    "revision": "5847d8b9f837d016cb74",
+    "url": "/home-ops/static/js/runtime-main.8a316c15.js"
+  }
+]);
